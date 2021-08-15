c56de50b8485f26f3b53f358909805408d8d508e
RC_Kanashii <cc1515781862@163.com>
2021-08-15 17:13:34 +0800

    init projects

---
+++ Java点菜系统/src/Dish.java
@@ -0,0 +1,15 @@
+/**
+ * @author rc_kanashii
+ * 2021-08-15  17:02
+ */
+public class Dish {
+    int id;
+    String name;
+    double price;
+
+    public Dish(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
